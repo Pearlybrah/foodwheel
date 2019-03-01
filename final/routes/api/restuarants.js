f88aa480fc12f29ff7c249f48a6c527fd62dfeae
Zachary Ferreira <Pearlybrah@github.com>
2019-02-28 23:13:40 -0800

    login and signin are working, next step is to display in components. This version of login has the bcrypt library incorporated

--- final/routes/api/restuarants.js
+++ final/routes/api/restuarants.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const router = express.Router();
 
 //Restuarant Model
 const Restaurant = require("../../models/Restuarant");
@@ -7,36 +6,36 @@ const Restaurant = require("../../models/Restuarant");
 // @route GET api/items
 // @desc GET All Items
 // @access Public
-router.get("/", (req, res) => {
-  Restaurant.find()
-    .sort({ date: -1 })
-    .then(restuarant => res.json(restuarant));
-});
-
-// @route POST api/items
-// @desc Create an Item
-// @access Public
-router.post("/", (req, res) => {
-  const newRestuarant = new Restaurant({
-    name: req.body.name
-    //add more fields here
+module.exports = app => {
+  app.get("/", (req, res) => {
+    Restaurant.find()
+      .sort({ date: -1 })
+      .then(restuarant => res.json(restuarant));
   });
-  newRestuarant.save().then(restuarant => res.json(restuarant));
-});
 
-// @route DELETE api/items
-// @desc Delete an Item
-// @access Public
-router.delete("/:id", (req, res) => {
-  Restaurant.findById(req.params.id).then(restuarant =>
-    restuarant
-      .remove()
-      .then(() =>
-        res
-          .json({ success: true })
-          .catch(err => res.status(404).json({ success: false }))
-      )
-  );
-});
+  // @route POST api/items
+  // @desc Create an Item
+  // @access Public
+  app.post("/", (req, res) => {
+    const newRestuarant = new Restaurant({
+      name: req.body.name
+      //add more fields here
+    });
+    newRestuarant.save().then(restuarant => res.json(restuarant));
+  });
 
-module.exports = router;
+  // @route DELETE api/items
+  // @desc Delete an Item
+  // @access Public
+  app.delete("/:id", (req, res) => {
+    Restaurant.findById(req.params.id).then(restuarant =>
+      restuarant
+        .remove()
+        .then(() =>
+          res
+            .json({ success: true })
+            .catch(err => res.status(404).json({ success: false }))
+        )
+    );
+  });
+};

08cc848cd13be960b01b8c2fa14c1157bb8e49ab
Danny D <dannyd2313@gmail.com>
2019-03-02 13:34:06 -0800

    APIsrc

---
+++ final/client/src2/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import DisplayPage from "./components/DisplayPage";
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <DisplayPage />
+      </div>
+    );
+  }
+}
+
+export default App;

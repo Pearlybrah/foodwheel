34af8954c30e8fd9de8afcb6b3033a53c9fcbe72
RDNRYPPLSPRSN <rdnrypplsprsn@protonmail.com>
2019-03-07 14:56:30 -0800

    Icon added

--- final/client/src/components/NavBar/NavBar.js
+++ final/client/src/components/NavBar/NavBar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Link } from "react-router-dom";
+import "./style.css";
 
 function NavBar() {
   return (
     <div>
-      <ul>
+      <ul className="nav-list">
         <li>
           <Link to="/mytable">My Table</Link>
         </li>
@@ -12,7 +13,6 @@ function NavBar() {
           <Link to="/account">My Account</Link>
         </li>
       </ul>
-      ))}
     </div>
   );
 }
